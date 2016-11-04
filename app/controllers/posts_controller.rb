84f885be268f57b10bd924992262a65b518c8c69
keilsmit <keilsmit@yahoo.com>
2016-11-04 17:09:23 -0400

    tttt

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
 
-before_action :require_login, only: [:new, :create, :index]
+before_action :require_login, only: [:new, :create]
 
   def index
     if current_user
